bd906c92d21d3bda908877f58e9f33b0cafe80ac
Lukas Jenicek <lukas.jenicek5@gmail.com>
2023-02-12 19:36:37 +0100

    Close server on interrupt signals + Hot reload of binaries

---
+++ Readme.md
@@ -0,0 +1,5 @@
+## Cookbook
+
+### Development
+1. Install [Air](https://github.com/cosmtrek/air) version 1.41.0
+2. Run `make run`
--- cmd/cookbook/main.go
+++ cmd/cookbook/main.go
@@ -1,14 +1,34 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/LukasJenicek/CookBook/services/api"
 )
 
 func main() {
-	err := http.ListenAndServe(":3000", api.Routes())
+	signals := make(chan os.Signal)
+	signal.Notify(signals, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	srv := http.Server{
+		Addr:    ":3000",
+		Handler: api.Routes(),
+	}
+
+	go func() {
+		select {
+		case sig := <-signals:
+			fmt.Printf("Got %s signal. Shutting down the server...\n", sig)
+			srv.Close()
+		}
+	}()
+
+	err := srv.ListenAndServe()
 	if err != nil {
 		log.Fatal(err)
 	}
